1619db2809b152a4ea07647d115dee9db8fcda11
Hardik <hk912701@gmail.com>
2020-11-13 06:46:02 +0000

    Revert "mm, oom: prefer thread group leaders for display purposes"
    This reverts commit 9624e6b16e163330131999447d5d5feebaee4259.

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -325,14 +325,10 @@ static struct task_struct *select_bad_process(unsigned int *ppoints,
 			break;
 		};
 		points = oom_badness(p, NULL, nodemask, totalpages);
-		if (!points || points < chosen_points)
-			continue;
-		/* Prefer thread group leaders for display purposes */
-		if (points == chosen_points && thread_group_leader(chosen))
-			continue;
-
-		chosen = p;
-		chosen_points = points;
+		if (points > chosen_points) {
+			chosen = p;
+			chosen_points = points;
+		}
 	}
 	if (chosen)
 		get_task_struct(chosen);
