3c1b115b2e345690671d9f3f1fc1b9d3ade49ce4
Hardik <hk912701@gmail.com>
2020-11-13 06:44:31 +0000

    Revert "vfree: don't schedule free_work() if llist_add() returns false"
    This reverts commit f676ea118213b5a7adc40e5fd0d45d35a3fd91d7.

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1572,6 +1572,7 @@ static void __vunmap(const void *addr, int deallocate_pages)
  *	conventions for vfree() arch-depenedent would be a really bad idea)
  *
  *	NOTE: assumes that the object at *addr has a size >= sizeof(llist_node)
+ *	
  */
 void vfree(const void *addr)
 {
@@ -1583,8 +1584,8 @@ void vfree(const void *addr)
 		return;
 	if (unlikely(in_interrupt())) {
 		struct vfree_deferred *p = &__get_cpu_var(vfree_deferred);
-		if (llist_add((struct llist_node *)addr, &p->list))
-			schedule_work(&p->wq);
+		llist_add((struct llist_node *)addr, &p->list);
+		schedule_work(&p->wq);
 	} else
 		__vunmap(addr, 1);
 }
