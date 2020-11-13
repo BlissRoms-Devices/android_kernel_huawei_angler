2b4046645562a7694d4d70ada01b818d921dd827
Hardik <hk912701@gmail.com>
2020-11-13 06:45:15 +0000

    Revert "oom: add helpers for setting and clearing TIF_MEMDIE"
    This reverts commit f2a9a1da653f283f258e4d78da58478e0da46528.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -170,12 +170,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			     free);
 
 		lowmem_deathpending_timeout = jiffies + HZ;
-		/*
-		 * FIXME: lowmemorykiller shouldn't abuse global OOM killer
-		 * infrastructure. There is no real reason why the selected
-		 * task should have access to the memory reserves.
-		 */
-		mark_tsk_oom_victim(selected);
+		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		send_sig(SIGKILL, selected, 0);
 		rem -= selected_tasksize;
 	}
--- kernel/exit.c
+++ kernel/exit.c
@@ -458,7 +458,7 @@ static void exit_mm(struct task_struct *tsk)
 	task_unlock(tsk);
 	mm_update_next_owner(mm);
 	mmput(mm);
-	unmark_oom_victim();
+	clear_thread_flag(TIF_MEMDIE);
 }
 
 static struct task_struct *find_alive_thread(struct task_struct *p)
