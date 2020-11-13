5463924c72140cdbbf77783fc232fc44eade940f
Hardik <hk912701@gmail.com>
2020-11-13 06:45:48 +0000

    Revert "wait: WSTOPPED|WCONTINUED doesn't work if a zombie leader is traced by another process"
    This reverts commit 7a22d0d1168ce5fad29a74d78c24f7c46dd6889e.

--- kernel/exit.c
+++ kernel/exit.c
@@ -1416,16 +1416,20 @@ static int wait_consider_task(struct wait_opts *wo, int ptrace,
 
 	/* slay zombie? */
 	if (p->exit_state == EXIT_ZOMBIE) {
-		/* we don't reap group leaders with subthreads */
-		if (!delay_group_leader(p)) {
-			/*
-			 * A zombie ptracee is only visible to its ptracer.
-			 * Notification and reaping will be cascaded to the
-			 * real parent when the ptracer detaches.
-			 */
-			if (unlikely(ptrace) || likely(!p->ptrace))
-				return wait_task_zombie(wo, p);
+		/*
+		 * A zombie ptracee is only visible to its ptracer.
+		 * Notification and reaping will be cascaded to the real
+		 * parent when the ptracer detaches.
+		 */
+		if (likely(!ptrace) && unlikely(p->ptrace)) {
+			/* it will become visible, clear notask_error */
+			wo->notask_error = 0;
+			return 0;
 		}
+
+		/* we don't reap group leaders with subthreads */
+		if (!delay_group_leader(p))
+			return wait_task_zombie(wo, p);
 
 		/*
 		 * Allow access to stopped/continued state via zombie by
