b746bffcd674dd67ac702826785f4373975528b1
Hardik <hk912701@gmail.com>
2020-11-13 06:45:23 +0000

    Revert "exit: reparent: introduce find_alive_thread()"
    This reverts commit c6f8eee18c08dd981468ef7b94f233fc1091b127.

--- kernel/exit.c
+++ kernel/exit.c
@@ -461,17 +461,6 @@ static void exit_mm(struct task_struct *tsk)
 	clear_thread_flag(TIF_MEMDIE);
 }
 
-static struct task_struct *find_alive_thread(struct task_struct *p)
-{
-	struct task_struct *t;
-
-	for_each_thread(p, t) {
-		if (!(t->flags & PF_EXITING))
-			return t;
-	}
-	return NULL;
-}
-
 static struct task_struct *find_child_reaper(struct task_struct *father)
 	__releases(&tasklist_lock)
 	__acquires(&tasklist_lock)
@@ -482,8 +471,9 @@ static struct task_struct *find_child_reaper(struct task_struct *father)
 	if (likely(reaper != father))
 		return reaper;
 
-	reaper = find_alive_thread(father);
-	if (reaper) {
+	for_each_thread(father, reaper) {
+		if (reaper->flags & PF_EXITING)
+			continue;
 		pid_ns->child_reaper = reaper;
 		return reaper;
 	}
@@ -509,13 +499,16 @@ static struct task_struct *find_child_reaper(struct task_struct *father)
 static struct task_struct *find_new_reaper(struct task_struct *father,
 					   struct task_struct *child_reaper)
 {
-	struct task_struct *thread, *reaper;
+	struct task_struct *thread;
 
-	thread = find_alive_thread(father);
-	if (thread)
+	for_each_thread(father, thread) {
+		if (thread->flags & PF_EXITING)
+			continue;
 		return thread;
+	}
 
 	if (father->signal->has_child_subreaper) {
+		struct task_struct *reaper;
 		/*
 		 * Find the first ->is_child_subreaper ancestor in our pid_ns.
 		 * We start from father to ensure we can not look into another
@@ -529,9 +522,10 @@ static struct task_struct *find_new_reaper(struct task_struct *father,
 				break;
 			if (!reaper->signal->is_child_subreaper)
 				continue;
-			thread = find_alive_thread(reaper);
-			if (thread)
-				return thread;
+			for_each_thread(reaper, thread) {
+				if (!(thread->flags & PF_EXITING))
+					return thread;
+			}
 		}
 	}
 
